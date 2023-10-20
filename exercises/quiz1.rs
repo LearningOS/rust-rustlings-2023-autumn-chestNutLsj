cc7f987542bbed0feb4c12c314f19093de608ba0
Senj Lee <chestnutlsj@gmail.com>
2023-10-20 17:40:11 +0800

    phase2

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num:i32)-> i32 {
+    let mut price:i32;
+    if num <=40{
+        price=2*num;
+    }else{
+        price=num;
+    }
+    return price;
+}
 
 // Don't modify this function!
 #[test]
